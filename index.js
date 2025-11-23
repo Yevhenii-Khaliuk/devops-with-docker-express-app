617f43a71ec93c18242e280ef1a06b114405ba7d
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2025-11-23 09:44:50 +0100

    Add additional message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World updated!</h1>");
+  res.send("<h1>Hello World!</h1><h2>Deployed via deploy hook.</h2>");
 });
 
 const PORT = 8080;
